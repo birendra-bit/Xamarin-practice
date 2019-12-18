596abcc411f663dd988afdc9080b9e8b5e2698d3
birendra-bit <bhujelbirendra3@gmail.com>
2019-12-18 21:35:58 +0530

    xamarin app developing practice

---
+++ XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        void OnButtonClicked( object sender, EventArgs e)
+        {
+               
+            (sender as Button).Text = "Click Me Again";
+        }
+        void OnEntryTextChanged( object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
