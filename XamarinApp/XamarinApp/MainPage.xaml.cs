cea05956b6876f8ae01d0247b162e21b18de54b1
birendra-bit <bhujelbirendra3@gmail.com>
2019-12-19 17:17:24 +0530

    listview implemented

--- XamarinApp/XamarinApp/MainPage.xaml.cs
+++ XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -9,19 +9,67 @@ namespace XamarinApp
 {
     public partial class MainPage : ContentPage
     {
-        void OnButtonClicked( object sender, EventArgs e)
-        {
-               
-            (sender as Button).Text = "Click Me Again";
-        }
-        void OnEntryTextChanged( object sender, TextChangedEventArgs e)
-        {
-            string oldText = e.OldTextValue;
-            string newText = e.NewTextValue;
-        }
+        //void OnButtonClicked( object sender, EventArgs e)
+        //{
+
+        //    (sender as Button).Text = "Click Me Again";
+        //}
+        //void OnEntryTextChanged( object sender, TextChangedEventArgs e)
+        //{
+        //    string oldText = e.OldTextValue;
+        //    string newText = e.NewTextValue;
+        //}
+        //void OnEntryComplete( object sender, EventArgs e)
+        //{
+        //    string text = ((Entry)sender).Text;
+        //}
+        public IList<PersonDeatils> Person { get; private set; }
         public MainPage()
         {
             InitializeComponent();
+            Person = new List<PersonDeatils>();
+
+            Person.Add(new PersonDeatils
+            {
+                Name = "Birendra Bhujel",
+                Designation = "Intern",
+                DateOfJoining = "02/09/2019",
+                ImageUrl = "download.jpg",
+                Description = "I like coding and visiting new places"
+            });
+            Person.Add(new PersonDeatils
+            {
+                Name = "Birendra Bhujel",
+                Designation = "Intern",
+                DateOfJoining = "02/09/2019",
+                ImageUrl = "download.jpg",
+                Description ="I like coding and visiting new places"
+            });
+            Person.Add(new PersonDeatils
+            {
+                Name = "Birendra Bhujel",
+                Designation = "Intern",
+                DateOfJoining = "02/09/2019",
+                ImageUrl = "download.jpg",
+                Description = "I like coding and visiting new places"
+            });
+            Person.Add(new PersonDeatils
+            {
+                Name = "Birendra Bhujel",
+                Designation = "Intern",
+                DateOfJoining = "02/09/2019",
+                ImageUrl = "download.jpg",
+                Description = "I like coding and visiting new places"
+            });
+            Person.Add(new PersonDeatils
+            {
+                Name = "Birendra Bhujel",
+                Designation = "Intern",
+                DateOfJoining = "02/09/2019",
+                ImageUrl = "download.jpg",
+                Description = "I like coding and visiting new places"
+            });
+            BindingContext = this;
         }
     }
 }
---
+++ XamarinApp/XamarinApp/PersonDeatils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinApp
+{
+    public class PersonDeatils
+    {
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string DateOfJoining { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        //public override string ToString()
+        //{
+        //    return Name;
+        //}
+    }
+}
